00d8e9b8cae422002794b37c1d4aaabf0f3de7d3
Yoan Mollard <ymollard@users.noreply.github.com>
2020-02-09 17:26:15 +0100

    Bugfix error display

--- luos_interface/luos_interface/modules/__init__.py
+++ luos_interface/luos_interface/modules/__init__.py
@@ -10,5 +10,5 @@ def make_module_interface_factory(node, module):
     if module.type in _make:
         return _make[module.type](node, module)
     else:
-        print(ValueError("Luos module type '{}' is unknown".format(type(module.type))))
+        print(ValueError("Luos module type '{}' is unknown".format(module.type)))
         #TODO raise ValueError("Luos module type '{}' is unknown".format(type(module.type)))
