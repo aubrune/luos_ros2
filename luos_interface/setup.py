9f959555fb8876fba01f8f9564504dd588cea363
Yoan Mollard <ymollard@users.noreply.github.com>
2020-02-11 16:52:56 +0100

    Added resource markers

--- luos_interface/setup.py
+++ luos_interface/setup.py
@@ -7,6 +7,8 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools', 'pyserial', 'pyluos==1.1.0'],
