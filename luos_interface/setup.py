e95b8f9db33a249bfd706bd2f96c4e2f8200ad15
Yoan Mollard <ymollard@users.noreply.github.com>
2020-05-03 00:27:39 +0200

    Version bump

--- luos_interface/setup.py
+++ luos_interface/setup.py
@@ -6,7 +6,7 @@ package_name = 'luos_interface'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
