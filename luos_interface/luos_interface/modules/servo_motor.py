ff165b9e7a21d0417d460c837ced89945b86428c
Yoan Mollard <ymollard@users.noreply.github.com>
2020-11-07 20:42:49 +0100

    angles in radians

--- luos_interface/luos_interface/modules/servo_motor.py
+++ luos_interface/luos_interface/modules/servo_motor.py
@@ -1,17 +1,17 @@
 from luos_msgs.msg import FloatChange
 from std_msgs.msg import Float32
-from .serializers import serializeFloat32, serializeFloatChange
-from .deserializers import deserializeFloat32
+from .serializers import serializeFloat32, serializeFloat32DegToRad
+from .deserializers import deserializeFloat32, deserializeFloat32RadToDeg
 from .generic import LuosGenericPublisher
 
 class LuosServoMotorPublisher(LuosGenericPublisher):
     def __init__(self, node, module, rate):
         variables = {
             "rot_position": {"read_type": Float32, "write_type": Float32,
-                      "serialize": serializeFloat32, "deserialize": deserializeFloat32,
+                      "serialize": serializeFloat32DegToRad, "deserialize": deserializeFloat32RadToDeg,
                       },
             "max_angle": {"read_type": Float32, "write_type": Float32,
-                      "serialize": serializeFloat32, "deserialize": deserializeFloat32,
+                      "serialize": serializeFloat32DegToRad, "deserialize": deserializeFloat32RadToDeg,
                       },
             "min_pulse": {"read_type": Float32, "write_type": Float32,
                       "serialize": serializeFloat32, "deserialize": deserializeFloat32,
